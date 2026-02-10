0bfea0639c83fb201d23eb39d6adab82b5d97c96
Yannick Spies <y.spies@inshared.nl>
2026-02-10 12:46:02 +0000

    add copilot.vim plugin for neovim

---
+++ nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  'github/copilot.vim',
+  event = 'InsertEnter',
+  config = function()
+    -- Accept suggestion with Ctrl+J (Tab conflicts with other plugins)
+    vim.g.copilot_assume_mapped = true
+    vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false, desc = 'Copilot Accept' })
+    vim.keymap.set('i', '<C-l>', '<Plug>(copilot-accept-word)', { desc = 'Copilot Accept Word' })
+    vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)', { desc = 'Copilot Next Suggestion' })
+    vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)', { desc = 'Copilot Previous Suggestion' })
+    vim.keymap.set('i', '<C-\\>', '<Plug>(copilot-dismiss)', { desc = 'Copilot Dismiss' })
+
+    -- Disable default Tab mapping to avoid conflicts with blink.cmp
+    vim.g.copilot_no_tab_map = true
+  end,
+}
